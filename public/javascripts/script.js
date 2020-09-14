08e0eeaf3cb1c7cb5095c1617f9e70a1fa6a7e5e
Jorell Langmar <jorell.langmar@gmail.com>
2020-09-14 15:36:09 +0100

    Bonus 1 done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,6 +6,8 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 	console.log(mashPotatoes[i]);
 }
 
+const table = document.querySelector("#table");
+
 // Iteration 1 using callbacks
 addFood(steak[0], "#steak", () => {
 	addFood(steak[1], "#steak", () => {
@@ -14,7 +16,9 @@ addFood(steak[0], "#steak", () => {
 				addFood(steak[4], "#steak", () => {
 					addFood(steak[5], "#steak", () => {
 						addFood(steak[6], "#steak", () => {
-							addFood(steak[7], "#steak", () => {});
+							addFood(steak[7], "#steak", () => {
+								table.innerHTML += `<img src="./public/images/steak.jpg" alt="steak">`;
+							});
 						});
 					});
 				});
@@ -28,7 +32,9 @@ addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
 	addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
 		addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
 			addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
-				addFood(mashPotatoes[4], "#mashPotatoes").then(() => {});
+				addFood(mashPotatoes[4], "#mashPotatoes").then(() => {
+          table.innerHTML += `<img src="./public/images/mashPotatoes.jpg" alt="steak">`
+        });
 			});
 		});
 	});
@@ -39,7 +45,10 @@ addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
 async function makeFood(step) {
 	for (let i = 0; i < step.length; i++) {
 		await addFood(step[i], `#brusselSprouts`);
-	}
+  };
+  table.innerHTML += `<img src="./public/images/brusselSprouts.jpg" alt="steak">`
 }
 
 makeFood(brusselSprouts);
+
+
